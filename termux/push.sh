b190d2888095aaf4bc3e928c6bdf5e41477aa0fb
CADORET Matthias <cadoret.matt@hotmail.com>
2025-11-16 11:15:21 +0100

    BashTermux

---
+++ termux/push.sh
@@ -0,0 +1,37 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+# --- CONFIG À MODIFIER ---
+CHEMIN1="/data/data/com.termux/files/home/github/"
+CHEMIN2="/data/data/com.termux/files/home/storage/documents/progdoc/"
+# --------------------------
+
+# Demande du nom du dossier
+echo -n "Nom du dossier : "
+read NOM
+
+DOSSIER1="$CHEMIN1/$NOM"
+DOSSIER2="$CHEMIN2/$NOM"
+
+# Vérification dossier
+if [ ! -d "$DOSSIER2" ]; then
+    echo "Erreur : $DOSSIER2 n'existe pas."
+    exit 1
+fi
+
+# Suppression dossier dans chemin1
+rm -rf "$DOSSIER1"
+
+# Copie depuis chemin2 vers chemin1
+cp -r "$DOSSIER2" "$DOSSIER1"
+
+# Git add / commit / push
+cd "$DOSSIER1" || exit
+git add .
+
+echo -n "Message du commit : "
+read MESSAGE
+
+git commit -m "$MESSAGE"
+git push
+
+echo "Synchronisation + push terminés."
